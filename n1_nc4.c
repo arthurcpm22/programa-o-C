3e228d73c166ac74e52be36f9c2fc761ea9c5e5e
arthurcpm22 <arthurpedretti@hotmail.com>
2025-09-02 10:15:41 -0300

    Create n1_nc4.c

---
+++ n1_nc4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 29;
+    int quantidade = 1;
+    float altura = 1.81;
+    double peso = 120.3;
+    char letra = 'A';
+    char nome[20] = "Arthur";
+
+    }
